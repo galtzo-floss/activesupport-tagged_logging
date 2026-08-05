04a56f71f4fba2435142ffa918a7f84f3a9596b1
Peter H. Boling <peter.boling@gmail.com>
2026-08-05 04:31:56 -0600

    🐛 Keep debug loading out of CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ require "minitest"
 require "test-unit"
 
 # RSpec Configs
-require "debug" if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.7")
+require "debug" if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.7") && ENV["CI"].nil? && ENV.fetch("DEBUG", "false").casecmp("true").zero?
 require "config/rspec/rspec_block_is_expected"
 require "config/rspec/rspec_core"
 require "config/rspec/version_gem"
