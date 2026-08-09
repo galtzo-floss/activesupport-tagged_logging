11ce0081c41b8a92297ce0ba740551888e842dfe
Peter H. Boling <peter.boling@gmail.com>
2026-08-08 21:07:19 -0600

    🔖 Bump gem version

--- lib/activesupport/tagged_logging/version.rb
+++ lib/activesupport/tagged_logging/version.rb
@@ -6,7 +6,7 @@ module ActiveSupport
       # Version namespace for this gem.
       module Version
         # Current gem version.
-        VERSION = "2.0.3"
+        VERSION = "2.0.4"
       end
       # Current gem version exposed at the traditional constant location.
       VERSION = Version::VERSION # Traditional Constant Location
